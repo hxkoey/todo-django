d6d8f32feef2b044ee97f516aecd523681b2a5de
hxkoey <koey.huixin@gmail.com>
2020-12-01 14:48:00 +0800

    secret_key

--- todo/settings.py
+++ todo/settings.py
@@ -136,5 +136,5 @@ STATICFILES_DIRS = [STATIC_DIR, ]
 # CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 # Configure Django App for Heroku
-import django_heroku
-django_heroku.settings(locals())
+#import django_heroku
+#django_heroku.settings(locals())
